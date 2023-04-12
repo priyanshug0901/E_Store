5b3934fa4add2518fd8e5ea271a5237df0e13e4d
priyanshug0901 <priyanshug0901@gmail.com>
2023-04-12 17:54:25 +0530

    added splash screen

--- app/src/main/java/com/android/e_mart/MainActivity.kt
+++ app/src/main/java/com/android/e_mart/MainActivity.kt
@@ -2,10 +2,13 @@ package com.android.e_mart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(1000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
